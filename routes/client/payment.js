7af8ee21d25ece2e8877855b9b579c54e867e076
agamurad <agamuradn@gmail.com>
2026-01-29 21:06:39 +0400

    feat: add stripe checkout session flow

--- routes/client/payment.js
+++ routes/client/payment.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const {
-  createPaymentIntent,
-  confirmPayment,
-} = require("../controllers/payment");
-
 const router = express.Router();
+const { createStripeCheckout } = require("../../controllers/payment");
 
-router.post("/create-intent", createPaymentIntent);
+router.post("/checkout", createStripeCheckout);
 
 module.exports = router;
